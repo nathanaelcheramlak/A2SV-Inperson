eea3d8e45b04dc2bf30932b865afc4143ee74796
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-01-30 21:59:33 +0300

    Created 'Find the Maximum Achievable Number' by A2SV Hub

---
+++ 30-Jan-2025/Find the Maximum Achievable Number 228361.py
@@ -0,0 +1,10 @@
+# Problem: Find the Maximum Achievable Number - https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        """
+        :type num: int
+        :type t: int
+        :rtype: int
+        """
+        return num + 2 * t
