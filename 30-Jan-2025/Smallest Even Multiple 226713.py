d6ebc255e5e3208c5051ad0f55689e9271310ae5
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-01-30 15:54:35 +0300

    Created 'Smallest Even Multiple' by A2SV Hub

---
+++ 30-Jan-2025/Smallest Even Multiple 226713.py
@@ -0,0 +1,7 @@
+# Problem: Smallest Even Multiple - https://leetcode.com/problems/smallest-even-multiple/
+
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n % 2 == 1:
+            return n * 2
+        return n
