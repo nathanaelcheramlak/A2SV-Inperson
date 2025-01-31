4b09a40d2beea67f6e4b0b0847d7bc6debb9552e
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-01-31 09:19:08 +0300

    Created 'Palindrome Number ' by A2SV Hub

---
+++ 31-Jan-2025/Palindrome Number  229648.py
@@ -0,0 +1,9 @@
+# Problem: Palindrome Number  - https://leetcode.com/problems/palindrome-number/
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return str(x) == str(x)[::-1]
