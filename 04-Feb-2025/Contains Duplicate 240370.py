fadbbdfdc3e38fad38b1b4f74ce587bf2022c3ab
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-02-04 17:29:22 +0300

    Created 'Contains Duplicate' by A2SV Hub

---
+++ 04-Feb-2025/Contains Duplicate 240370.py
@@ -0,0 +1,5 @@
+# Problem: Contains Duplicate - https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
