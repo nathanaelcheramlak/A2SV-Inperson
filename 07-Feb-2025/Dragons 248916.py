15812eb22e74f192406e291cd6a7b2c8506b899d
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-02-07 09:32:55 +0300

    Created 'Dragons' by A2SV Hub

---
+++ 07-Feb-2025/Dragons 248916.py
@@ -0,0 +1,19 @@
+# Problem: Dragons - https://codeforces.com/problemset/problem/230/A
+
+s, n = map(int, input().split())
+strength = list()
+
+for _ in range(n):
+    strength.append(list(map(int, input().split())))
+
+strength.sort()
+total_strength = s
+
+for dragon_strength, bonus_strength in strength:
+    if total_strength <= dragon_strength:
+        print("NO")
+        break
+    else:
+        total_strength += bonus_strength
+else:
+    print("YES")
