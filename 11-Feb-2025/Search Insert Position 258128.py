a28cede06cc44a45fc8e6fb66ac42c1e0b9dc3f3
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-02-11 22:29:31 +0300

    Created 'Search Insert Position' by A2SV Hub

---
+++ 11-Feb-2025/Search Insert Position 258128.py
@@ -0,0 +1,16 @@
+# Problem: Search Insert Position - https://leetcode.com/problems/search-insert-position/
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while right >= left:
+            mid = (left + right) // 2
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else: 
+                return mid
+        
+        return left
