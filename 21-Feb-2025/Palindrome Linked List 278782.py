cce580c7d3a96fc55d08a526115333d2a4200f28
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-02-21 17:27:48 +0300

    Created 'Palindrome Linked List' by A2SV Hub

---
+++ 21-Feb-2025/Palindrome Linked List 278782.py
@@ -0,0 +1,15 @@
+# Problem: Palindrome Linked List - https://leetcode.com/problems/palindrome-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        stack = []
+        while head:
+            stack.append(head.val)
+            head = head.next
+        
+        return stack == stack[::-1]
