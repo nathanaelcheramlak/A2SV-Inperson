1cbc7611719edaf94d914c19006d0cc5e5965ec2
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-02-24 08:53:42 +0300

    Created 'E - Symmetrization' by A2SV Hub

---
+++ A2SV G6 - Round #2 24-Feb-2025/E - Symmetrization 281456.py
@@ -0,0 +1,26 @@
+# Problem: E - Symmetrization - https://codeforces.com/gym/586960/problem/E
+
+from collections import Counter
+t = int(input())
+for _ in range(t):
+    row = int(input())
+    col = row
+    matrix = list()
+    answer = 0
+
+    for _ in range(row):
+        matrix.append(list(map(int, input().strip())))
+    
+    mat9 = list(list(x) for x in zip(*matrix[::-1]))
+    mat18 = list(list(x) for x in zip(*mat9[::-1]))
+    mat27 = list(list(x) for x in zip(*mat18[::-1]))
+
+    for i in range(row):
+        for j in range(col):
+            x = matrix[i][j] + mat9[i][j] + mat18[i][j] + mat27[i][j]
+            if x == 1 or x == 3:
+                answer += 1
+            elif x == 2:
+                answer += 2
+    
+    print(answer // 4)
