9d9ae4e7eb7ae0ac8d3a63a883a14e1c5c504190
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-02-27 23:02:47 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 27-Feb-2025/Fox And Snake 286475.py
@@ -0,0 +1,12 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+n, m = map(int, input().split())
+
+for i in range(n):
+    if (i + 1) % 2 == 1:
+        print('#' * m)
+    else:
+        if (i + 1) % 4 == 0:
+            print('#' + '.' * (m - 1))
+        else:
+            print('.' * (m - 1) + '#')
