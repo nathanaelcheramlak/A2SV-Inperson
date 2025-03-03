152074a5173c02557efcdf00b8023f55b9edd8cb
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-03-03 15:33:35 +0300

    Created 'Design browser history ' by A2SV Hub

---
+++ 03-Mar-2025/Design browser history  288863.py
@@ -0,0 +1,40 @@
+# Problem: Design browser history  - https://leetcode.com/problems/design-browser-history/
+
+class ListNode:
+
+    def __init__(self, url):
+        self.url = url
+        self.next = None
+        self.prev = None
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.current = ListNode(homepage)
+
+    def visit(self, url: str) -> None:
+        Node = ListNode(url)
+        self.current.next = Node
+        Node.prev = self.current
+        self.current = self.current.next
+
+    def back(self, steps: int) -> str:
+        for _ in range(steps):
+            if not self.current.prev:
+                return self.current.url
+            self.current = self.current.prev
+        return self.current.url
+
+    def forward(self, steps: int) -> str:
+        for _ in range(steps):
+            if not self.current.next:
+                return self.current.url
+            self.current = self.current.next
+        return self.current.url
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
