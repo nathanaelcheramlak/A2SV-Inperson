190147475dbe9277e463cac9f283032120656a9f
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-03-05 19:49:31 +0300

    Created 'Crawler Log Folder' by A2SV Hub

---
+++ 05-Mar-2025/Crawler Log Folder 293387.py
@@ -0,0 +1,16 @@
+# Problem: Crawler Log Folder - https://leetcode.com/problems/crawler-log-folder/
+
+class Solution(object):
+    def minOperations(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: int
+        """
+        op_needed = 0
+        for log in logs:
+            if log == '../':
+                op_needed = max(0, op_needed - 1)
+            elif log != './':
+                op_needed += 1
+        
+        return op_needed
