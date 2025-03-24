ae151515a8580afde9373922d28dffb5009bbeeb
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-03-24 22:47:17 +0300

    Created 'Christmas Spruce' by A2SV Hub

---
+++ 24-Mar-2025/Christmas Spruce 317289.py
@@ -0,0 +1,29 @@
+# Problem: Christmas Spruce - https://codeforces.com/contest/913/problem/B
+
+from collections import defaultdict, deque
+import sys
+n = int(input())
+
+adj = defaultdict(list)
+for i in range(n - 1):
+    par = int(input())
+    adj[par].append(i + 2)
+
+level = [1]
+while level:
+    new_level = []
+    for node in level:
+        leaf_count = 0
+        for child in adj[node]:
+            if child not in adj:
+                leaf_count += 1
+            else:
+                new_level.append(child)
+
+        if leaf_count < 3:
+            print("No")
+            sys.exit(0)
+    level = new_level
+
+print('Yes')
+
