bb85ee87444f63e48a8796d1154df823eb0674ed
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-03-28 11:48:54 +0300

    Created 'E - Minimum Subsequence' by A2SV Hub

---
+++ A2SV G6 - Round #6 28-Mar-2025/E - Minimum Subsequence 320708.py
@@ -0,0 +1,28 @@
+# Problem: E - Minimum Subsequence - https://codeforces.com/gym/594077/problem/E
+
+for _ in range(int(input())):
+    n = int(input().strip())
+    seq = input().strip()
+
+    s = [0] * n
+    zero = [] 
+    one = []
+    
+    for index, char in enumerate(seq):
+        new = len(zero) + len(one)
+        if char == '0':
+            if one:
+                new = one.pop()
+            zero.append(new)
+        else:
+            if zero:
+                new = zero.pop()
+            one.append(new)
+        
+        s[index] = new
+
+    total = len(zero) + len(one)
+    print(total)
+    print(" ".join(str(idx + 1) for idx in s))
+
+
