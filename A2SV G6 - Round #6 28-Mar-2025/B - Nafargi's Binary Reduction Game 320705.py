b45a02048f46b3a6943244d9028edf306adaf8c6
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-03-28 11:48:33 +0300

    Created 'B - Nafargi's Binary Reduction Game' by A2SV Hub

---
+++ A2SV G6 - Round #6 28-Mar-2025/B - Nafargi's Binary Reduction Game 320705.py
@@ -0,0 +1,8 @@
+# Problem: B - Nafargi's Binary Reduction Game - https://codeforces.com/gym/594077/problem/B
+
+from collections import Counter
+n = int(input())
+binary = list(map(str, input().strip()))
+
+count = Counter(binary)
+print(abs(count['0'] - count['1']))
