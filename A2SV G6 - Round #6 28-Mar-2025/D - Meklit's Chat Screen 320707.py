ffd04c8a6cfa9e688b216a9cdb9205a1eeeff9d7
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-03-28 11:48:48 +0300

    Created 'D - Meklit's Chat Screen' by A2SV Hub

---
+++ A2SV G6 - Round #6 28-Mar-2025/D - Meklit's Chat Screen 320707.py
@@ -0,0 +1,28 @@
+# Problem: D - Meklit's Chat Screen - https://codeforces.com/gym/594077/problem/D
+
+from collections import deque, defaultdict
+
+n, k = map(int, input().split())
+chats = list(map(int, input().split()))
+
+q = deque()
+s = defaultdict(int)
+
+for i in range(min(n, k)):
+    if chats[i] not in s:
+        s[chats[i]] += 1
+        q.append(chats[i])
+
+for i in range(k,  n):
+    if chats[i] not in s:
+        s[chats[i]] += 1
+        q.append(chats[i])
+    if len(q) > k:
+        left = q.popleft()
+        s[left] -= 1
+        if s[left] == 0:
+            del s[left]
+
+q = list(q)[::-1]
+print(len(q))
+print(*q)
