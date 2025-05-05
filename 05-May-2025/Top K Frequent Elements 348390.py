adbd706871267bf811a3d69030744cee3bd9f551
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-05-05 09:58:19 +0300

    Created 'Top K Frequent Elements' by A2SV Hub

---
+++ 05-May-2025/Top K Frequent Elements 348390.py
@@ -0,0 +1,12 @@
+# Problem: Top K Frequent Elements - https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        heap = []
+        for num, freq in count.items():
+            heapq.heappush(heap, (freq, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [num for freq, num in heap]
